49e3722e7fc7cb22fb317f07900f96a747e7ee07
VarshaDevi21 <khvarshadevi555@gmail.com>
2026-08-09 21:30:59 +0530

    Configure CORS for Vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -23,10 +23,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
+        "https://grow-more-6uygaqeuk-growmore-interview.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
